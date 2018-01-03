2f0797c8e5de1d6c890b08f1987ec3958f7bf603
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-01-03 22:48:26 +1300

    created new icons for the languages and updated the seed data

--- seeds/languages.js
+++ seeds/languages.js
@@ -6,9 +6,9 @@ exports.seed = function (knex, Promise) {
       // Inserts seed entries
       return knex('languages').insert([
         {id: 1, language: 'Samoan', image: '/images/Samoa.png'},
-        { id: 2, language: 'Tongan', image: '/images/Tongan.jpg'},
+        { id: 2, language: 'Tongan', image: '/images/Tongan.png'},
         { id: 3, language: 'Fijian', image: '/images/Fiji.png'},
-        { id: 4, language: 'Maori', image: '/images/Maori.jpg'},
+        { id: 4, language: 'Maori', image: '/images/Maori.png'},
         { id: 5, language: 'Vanuatu', image: '/images/Vanuatu.png'}
       ])
     })

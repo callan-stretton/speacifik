4ef7cdf864bfded98f6dea52608cc54595bddf23
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-03-22 23:34:25 +1300

    defined propTypes

--- client/components/LanguageComp/Translate.jsx
+++ client/components/LanguageComp/Translate.jsx
@@ -20,11 +20,27 @@ class Translate extends React.Component {
       transcriptStarted: false
     }
   }
+
+  componentWillReceiveProps ({ finalTranscript, transcript, randomVid, dispatch, round }) {
+    if (finalTranscript.length && !this.state.finished) { // IF STATETMENT: if finalTranscript.length is truthy and the state of finished is false
+      this.setState({ finished: true }) // set the state of finished to true (to not exceed call stack)
+      this.compareFinalTranscript(finalTranscript)
+    } else if (transcript.length && !this.state.transcriptStarted) {
+      this.setState({ transcriptStarted: true })
+    }
+  }
+
   render () {
+    const { transcript, startListening, stopListening, resetTranscript, browserSupportsSpeechRecognition } = this.props
+    if (!browserSupportsSpeechRecognition) {
+      return null
+    }
     return <div>
       <h3>I am the Translate component</h3>
     </div>
   }
 }
+
+Translate.propTypes = propTypes
 
 export default Translate
